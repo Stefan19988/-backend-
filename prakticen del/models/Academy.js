81befc699e35e436a7b4d1e627530e93e9aefc70
Stefan19988 <mindc5043@gmail.com>
2025-05-18 14:33:20 +0200

    Delete prakticen del directory

--- prakticen del/models/Academy.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const AcademySchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  address: { type: String, required: true }
-});
-
-module.exports = mongoose.model('Academy', AcademySchema);
--- prakticen del/models/Course.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const CourseSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  address: { type: String, required: true },
-  area: { type: String, required: true }
-});
-
-module.exports = mongoose.model('Course', CourseSchema);
